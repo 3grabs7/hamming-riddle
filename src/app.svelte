8d85f543f3a7edbcaef57fa0e8c5a537963df888
Robin Grabs <robinmgrabs@gmail.com>
2022-04-30 22:34:53 +0200

    feat: main app component

---
+++ src/app.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import HammingCodeContainer from "./components/hamming-code-container.svelte";
+import bitService from "./scripts/bit.stream";
+
+	$: appState = bitService.appState$;
+</script>
+
+<main >
+	<h1>{$appState}</h1>
+	<HammingCodeContainer />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		position: relative;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
