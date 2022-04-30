e083ad49e5c5e7beb2e1ac78ae77ffdfb028a5e7
Robin Grabs <robinmgrabs@gmail.com>
2022-04-30 22:31:51 +0200

    feat: bit abstract base class

---
+++ src/models/bit.class.ts
@@ -0,0 +1,25 @@
+import type { BitType, BitValue as BitValue, ParityIndex } from './bit.types';
+import type { DataIndex } from './bit.types';
+
+export abstract class Bit {
+  containsKey: boolean = false;
+  position: ParityIndex | DataIndex;
+  value: BitValue;
+
+  abstract type: BitType;
+  abstract checkIfCorrect(): boolean;
+
+  constructor(position: ParityIndex | DataIndex) {
+    this.position = position;
+    this.setRandomValue();
+  }
+
+  flip(): void {
+    this.value = this.value === 1 ? 0 : 1;
+  }
+
+  setRandomValue(): void {
+    const randomBitValue = Math.round(Math.random());
+    this.value = randomBitValue as BitValue;
+  }
+}
